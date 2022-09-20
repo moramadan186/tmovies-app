55a44e914eb488932a6c7b9114b9ac1001447622
Mohamed Ramadan <mr01028760097@gmail.com>
2022-09-20 11:54:49 +0200

    project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import AppRoutes from './routes/AppRoutes';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <AppRoutes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from './theme/globalStyle';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme/theme';
+import "swiper/css/bundle";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
